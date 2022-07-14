c2547af6ea250c6a440677d96ee7bde5805e76b4
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-07-14 07:17:04 -0300

    atualizando jogo

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,10 +1,28 @@
 const X = "X";
 const O = "O";
 
+let jogador=X;
+let jogadas=1;
+let posicao=[ ["","",""],
+              ["","",""], 
+              ["","",""]   
+            ]
+
+function checaJogadas(){
+    if (jogadas%2<0){
+        jogador=X;
+    } else {
+        jogador=O;
+    }
+}
+
+
+
 function reiniciarJogo() {
     
 }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
+
 
 }
