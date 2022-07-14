dd323680b603507c2a5b972f2ebe6fcea0a6f37c
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-07-14 14:28:14 -0300

    Update jogo-velha.js

--- jogo-velha.js
+++ jogo-velha.js
@@ -4,10 +4,18 @@ const O = "O";
 let jogador=X;
 let jogadas=1;
 let jogo=true;
-let posicao=[ ["","",""],
-              ["","",""], 
-              ["","",""]   
-            ]
+let posicao=[]
+
+posicaoMatriz(posicao,'');
+
+function posicaoMatriz(posicao,valor){
+    for (let linha = 0; linha < 3; linha++) {
+        posicao[linha] = [];
+        for (let coluna = 0; coluna < 3; coluna++) {
+            posicao[linha][coluna] = valor;
+        }
+    }
+}
 
 function checaJogador(){
     if (jogo){
@@ -19,17 +27,42 @@ function checaJogador(){
     }
 }
 
-function reiniciarJogo() {
-    window.location.reload();
-}
-
-function selecionarArea(posicaoLinha, posicaoColuna) {
+function validaJogadas(posicaoLinha,posicaoColuna){
     if (posicao[posicaoLinha-1], [posicaoColuna-1]){
         desenharSimbolo(jogador, posicaoLinha, posicaoColuna);
     }
     jogadas++
     checaJogador();
-        
+}
+
+function reiniciarJogo() {
+    window.location.reload();
+}
+
+function selecionarArea(posicaoLinha, posicaoColuna) {
+    validaJogadas(posicaoLinha,posicaoColuna);
 }
 
 
+function horizontal() {
+    if (posicao[0][0]==jogador && posicao[0][1]==jogador && posicao[0][2]==jogador ||
+        posicao[1][0]==jogador && posicao[1][1]==jogador && posicao[1][2]==jogador ||
+        posicao[2][0]==jogador && posicao[2][1]==jogador && posicao[2][2]==jogador) {
+            exibirResultado(jogador)
+        }
+    
+}
+
+function vertical() {
+    if (posicao[0][0]==jogador && posicao[1][0]==jogador && posicao[2][0]==jogador ||
+        posicao[0][1]==jogador && posicao[1][1]==jogador && posicao[2][1]==jogador ||
+        posicao[0][2]==jogador && posicao[1][2]==jogador && posicao[2][2]==jogador) {
+            exibirResultado(jogador)
+        }
+    //for (let coluna = 0; coluna < 3; coluna++) {
+      //  if (posicao[0][coluna] == posicao[linha][1] && tabelaJogadas[linha][1] == posicao[linha][2] && posicao[linha][1] != '') {
+        //    exibirResultado("O vencedor é " + jogador);
+          //  jogo = false;
+        //}
+    //}
+}
