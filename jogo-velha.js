bf69f8cd80414d9d2430a6a258b6efdbd46bfb31
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-07-14 22:05:37 -0300

    Update jogo-velha.js

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,89 +1,207 @@
 const X = "X";
 const O = "O";
 
-let jogador=X;
-let jogadas=1;
-let jogo=true;
-let posicao=[]
+let jogador= X;
+let quantidadeJogadas = 0;
+let posicaoX = [];
+let posicaoO = [];
 
-posicaoMatriz(posicao,'');
-
-function posicaoMatriz(posicao,valor){
-    for (let linha = 0; linha < 3; linha++) {
-        posicao[linha] = [];
-        for (let coluna = 0; coluna < 3; coluna++) {
-            posicao[linha][coluna] = valor;
-        }
+function posicaoArea(posicaoLinha, posicaoColuna,jogador){
+    if(jogador == X){
+        posicaoX.push([posicaoLinha,posicaoColuna]);
     }
-}
+
+    if(jogador == O){
+        posicaoO.push([posicaoLinha,posicaoColuna]);
+    }
+ }
 
 function checaJogador(){
-    if (jogo){
-        if (jogadas%2!=0){
-            jogador=X;
-        } else {
-            jogador=O;
-        }
+    if (jogador == X){
+        jogador = O;
+        return marcarJogadorAtivo(jogador); 
     }
+    jogador = X;
+    return marcarJogadorAtivo(jogador);
 }
 
-function validaJogadas(posicaoLinha,posicaoColuna){
-    if (posicao[posicaoLinha-1], [posicaoColuna-1]){
-        desenharSimbolo(jogador, posicaoLinha, posicaoColuna);
-    }
-    jogadas++
-    checaJogador();
-    marcarJogadorAtivo(jogador);
-}
 
 function reiniciarJogo() {
     window.location.reload();
 }
 
-function selecionarArea(posicaoLinha, posicaoColuna) {
-    validaJogadas(posicaoLinha,posicaoColuna);
-    verificaVencedor();
+function verificaEmapate(){
+    quantidadeJogadas+=1;
+    console.log(quantidadeJogadas);
+    if(quantidadeJogadas == 9){
+        return exibirResultado("O JOGO EMPATOU");
+    }
 }
 
-function verificaVencedor(){
-    horizontal();
-    vertical();
-    diagonal();
-    empate();
+function verificaVencedor(posicaoX, posicaoO){
+    horizontal(posicaoX);
+    horizontal(posicaoO);
+    vertical(posicaoX);
+    vertical(posicaoO);
+    diagonal(posicaoX);
+    diagonal(posicaoO);
 }
 
-function horizontal() {
-    if (posicao[0][0]==jogador && posicao[0][1]==jogador && posicao[0][2]==jogador ||
-        posicao[1][0]==jogador && posicao[1][1]==jogador && posicao[1][2]==jogador ||
-        posicao[2][0]==jogador && posicao[2][1]==jogador && posicao[2][2]==jogador) {
-            exibirResultado(jogador)
-        }
+
+function horizontal(posicao) {
+    let quantidadeLinha1 = 0;
+    let quantidadeLinha2 = 0;
+    let quantidadeLinha3 = 0;
+
+
+    tamanhoArray = posicao.length;
+    for(i=0;i< tamanhoArray;i++){ 
+        //verifica se é a primeira linha
+        if(posicao[i][0] == 1){
+            if(posicao[i][1] == 1){
+                quantidadeLinha1+=1;
+            }
+            if(posicao[i][1] == 2){
+                quantidadeLinha1+=1;
+            }
+            if(posicao[i][1] == 3){
+                quantidadeLinha1+=1;
+            }
+            if(quantidadeLinha1 ==3){
+                return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+            }
+        }  
     
+        //linha 2
+        if(posicao[i][0] == 2){
+            if(posicao[i][1] == 1){
+                quantidadeLinha2+=1;
+            }
+            if(posicao[i][1] == 2){
+                quantidadeLinha2+=1;
+     
+             }
+            if(posicao[i][1] == 3){
+                quantidadeLinha2+=1;
+            }
+            if(quantidadeLinha2 == 3){
+                return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+            }
+     }  
+    
+        //linha 3
+        if(posicao[i][0] == 3){
+            if(posicao[i][1] == 1){
+                quantidadeLinha3+=1;
+            }
+            if(posicao[i][1] == 2){
+                quantidadeLinha3+=1;
+            }
+            if(posicao[i][1] == 3){
+                quantidadeLinha3+=1;
+            }
+            if(quantidadeLinha3 == 3){
+                return  exibirResultado("JOGADOR VENCEDOR: "+jogador);
+            }
+        }  
+    }
 }
 
-function vertical() {
-    if (posicao[0][0]==jogador && posicao[1][0]==jogador && posicao[2][0]==jogador ||
-        posicao[0][1]==jogador && posicao[1][1]==jogador && posicao[2][1]==jogador ||
-        posicao[0][2]==jogador && posicao[1][2]==jogador && posicao[2][2]==jogador) {
-            exibirResultado(jogador)
-        }
-}
+function vertical(posicao) {
+    let quantidadeColuna1 = 0;
+    let quantidadeColuna2 = 0;
+    let quantidadeColuna3 = 0;
 
-function diagonal() {
-    if (posicao[0][0]==jogador && posicao[1][1]==jogador && posicao[2][2]==jogador ||
-        posicao[0][2]==jogador && posicao[1][1]==jogador && posicao[2][0]==jogador) {
-            exibirResultado(jogador)
+    tamanhoArrayColuna = posicao.length;
+    for(i=0;i< tamanhoArrayColuna;i++){ 
+        //coluna1
+        if(posicao[i][1] == 1){
+            if(posicao[i][1] == 1){
+                quantidadeColuna1+=1;
+            }
+            if(posicao[i][1] == 2){
+                quantidadeColuna1+=1;
+            }
+            if(posicao[i][1] == 3){
+                quantidadeColuna1+=1;
+            }
+            if(quantidadeColuna1 == 3){
+                return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+            }
         }
-}
+        //coluna2
+        if(posicao[i][1] == 2){
+            if(posicao[i][1] == 1){
+                quantidadeColuna2+=1;
+            }
+            if(posicao[i][1] == 2){
+                quantidadeColuna2+=1;
+            }
+            if(posicao[i][1] == 3){
+                quantidadeColuna2+=1;
+            }
+            if(quantidadeColuna2 == 3){
+                return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+            }
+        }
 
-function empate(){
-    let jogoCompleto=0;
-    for (let linha=0;linha<3;linha++){
-        if (posicao[linha][coluna] != ''){
-            jogoCompleto++;
-        }
-        if (jogoCompleto==9){
-            jogo=false;
+        //coluna3
+        if(posicao[i][1] == 3){
+            if(posicao[i][1] == 1){
+                quantidadeColuna3+=1;
+            }
+            if(posicao[i][1] == 2){
+                quantidadeColuna3+=1;
+            }
+            if(posicao[i][1] == 3){
+                quantidadeColuna3+=1;
+            }
+            if(quantidadeColuna3 == 3){
+                return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+            }
         }
     }
+}
+
+function diagonal(posicao) {
+    let quantidadeDiagonal1 = 0;
+    let quantidadeDiagonal2 = 0;
+
+    tamanhoArrayDiagonal = posicao.length;
+    for(i=0;i< tamanhoArrayDiagonal;i++){ 
+        
+        if(posicao[i][0] == 1 && posicao[i][1]==3){
+            quantidadeDiagonal1+=1;
+        }
+        if(posicao[i][0] == 2 && posicao[i][1]==2){
+            quantidadeDiagonal1+=1;
+        }
+        if(posicao[i][0] == 3 && posicao[i][1]==1){
+            quantidadeDiagonal1+=1;
+        }
+        if(quantidadeDiagonal1 == 3){
+            return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+        }
+
+        if(posicao[i][0] == 1 && posicao[i][1]==1){
+            quantidadeDiagonal2+=1;
+        }
+        if(posicao[i][0] == 2 && posicao[i][1]==2){
+            quantidadeDiagonal2+=1;
+        }
+        if(posicao[i][0] == 3 && posicao[i][1]==3){
+            quantidadeDiagonal2+=1;
+        }
+        if(quantidadeDiagonal2 == 3){
+            return exibirResultado("JOGADOR VENCEDOR: "+jogador);
+        }
+    }
+}
+
+function selecionarArea(posicaoLinha, posicaoColuna) {
+    posicaoArea(posicaoLinha, posicaoColuna,jogador);
+    desenharSimbolo(jogador, posicaoLinha, posicaoColuna);
+    verificaEmapate();
+    verificaVencedor(posicaoX, posicaoO);
+    checaJogador();
 }
